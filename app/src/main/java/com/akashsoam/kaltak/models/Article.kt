2b56f8ba92215e67c5aad70f95c343e5f915b5a6
soam1 <soamakash9@gmail.com>
2024-08-27 20:22:46 +0530

    db setup using room

--- app/src/main/java/com/akashsoam/kaltak/models/Article.kt
+++ app/src/main/java/com/akashsoam/kaltak/models/Article.kt
@@ -1,12 +1,20 @@
 package com.akashsoam.kaltak.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "articles")
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
     val author: String,
-    val content: Any,
-    val description: Any,
+    val content: String,
+    val description: String,
     val publishedAt: String,
+    //since source data type is not supported by our sqlite db, we will store it as string using type converter
     val source: Source,
     val title: String,
     val url: String,
     val urlToImage: Any
-)
+): Serializable
