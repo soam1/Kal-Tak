1d5a4283d80d922989acbb0dce8bfa1198e5008c
soam1 <soamakash9@gmail.com>
2024-08-29 00:31:58 +0530

    both bugs fixed

--- app/src/main/java/com/akashsoam/kaltak/ui/fragments/HeadlinesFragment.kt
+++ app/src/main/java/com/akashsoam/kaltak/ui/fragments/HeadlinesFragment.kt
@@ -67,7 +67,7 @@ class HeadlinesFragment : Fragment(R.layout.fragment_headlines) {
                     hideProgressBar()
                     hideErrorMessage()
                     response.data?.let { newsResponse ->
-                        newsAdapter.differ.submitList(newsResponse.articles.toList())
+                        newsAdapter.differ.submitList(newsResponse.articles)
                         val totalPages = newsResponse.totalResults / Constants.QUERY_PAGE_SIZE + 2
                         isLastPage = newsViewModel.headlinesPage == totalPages
                         if (isLastPage) {
